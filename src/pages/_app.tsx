116e6dbbc137fcafc733ca545f3efcb69abd8c4c
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-03-26 15:30:18 -0500

    [chore] Init Project and installed react-intl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useRouter, NextRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+import type { ComponentType } from "react";
+import es from "../lang/es.json";
+import en from "../lang/en.json";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+interface MyAppProps {
+  Component: ComponentType<any>;
+  pageProps: any;
 }
+
+interface Messages {
+  [key: string]: Record<string, string>;
+}
+
+function MyApp({ Component, pageProps }: MyAppProps) {
+  const { locale }: NextRouter = useRouter();
+  const messages: Messages = { es, en };
+  return (
+    <IntlProvider locale={locale ?? "en"} messages={messages[locale ?? "en"]}>
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
+}
+
+export default MyApp;
