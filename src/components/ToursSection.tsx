fe07b5d5e040ffe1d20932aa2da4355263321f29
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-03-27 22:01:24 -0500

    [chore] Add Translations for Tours List

--- src/components/ToursSection.tsx
+++ src/components/ToursSection.tsx
@@ -1,17 +1,16 @@
 import TourCard from "./TourCard";
 import { tours } from "@/utils/tours";
+import { FormattedMessage } from "react-intl";
 
 const TourSection = () => {
   return (
     <>
       <div className='pt-20 flex flex-col justify-center items-center w-full px-20'>
         <h2 className='text-3xl font-fjalla text-primary uppercase mb-5'>
-          Our Bike Tours
+          <FormattedMessage id='home.tours.title' />
         </h2>
         <p className='text-lg'>
-          We leave during the week by reservation and on the weekend by call.
-          You can find the call in our Bike Tour calendar published in the
-          highlights of Instragram.
+          <FormattedMessage id='home.tours.description' />
         </p>
       </div>
 
