eb869eb681d3735507c75de5f006f12428edaa07
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-03-29 13:23:39 -0500

    [bug] Fix Amplify bug - Add base Path to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "https://main.d2e6vubu8fnplc.amplifyapp.com/",
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
