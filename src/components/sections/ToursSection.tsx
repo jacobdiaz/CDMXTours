ecde9af4c1fd07497c6d4fc54e379aa8bfb9c370
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-04-08 14:13:40 -0500

    [feat] Add Blog page place holder

--- src/components/sections/ToursSection.tsx
+++ src/components/sections/ToursSection.tsx
@@ -3,17 +3,21 @@ import { Tours } from "@/utils/toursdata";
 import { FormattedMessage } from "react-intl";
 import SectionTitle from "./SectionTitle";
 
-const TourSection = () => (
+const TourSection = ({ hideHeader = false }: { hideHeader?: boolean }) => (
   <>
-    <div className='flex flex-col justify-center md:items-center mt-10 mb-5'>
-      <SectionTitle margin='m-0'>
-        <FormattedMessage id='home.tours.title' />
-      </SectionTitle>
-    </div>
+    {!hideHeader && (
+      <div className='flex flex-col justify-center md:items-center mt-10 mb-5'>
+        <SectionTitle margin='m-0'>
+          <FormattedMessage id='home.tours.title' />
+        </SectionTitle>
+      </div>
+    )}
     <div className='flex flex-col justify-center items-center w-full'>
-      <p className='text-lg mb-10'>
-        <FormattedMessage id='home.tours.description' />
-      </p>
+      {!hideHeader && (
+        <p className='text-lg mb-10'>
+          <FormattedMessage id='home.tours.description' />
+        </p>
+      )}
       <ul className='flex flex-wrap justify-center md:justify-between w-full'>
         {Tours.map((t) => (
           <TourCard {...t} key={t.id.toString()} />
