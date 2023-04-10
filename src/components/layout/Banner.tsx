c25e9240f2ce0cb1fa943d179495bfa46a7cb997
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-04-10 16:21:40 -0500

    [chore] Lazy load Banner component

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -1,18 +1,19 @@
+import Image from "next/image";
+import ImageOverlay from "./ImageOverlay";
+
 type BannerProps = {
-  background: string;
-  children?: React.ReactNode;
+  src: string;
+  alt: string;
+  children: React.ReactNode;
 };
 
-const Banner = ({ background, children }: BannerProps) => (
-  <div
-    className='h-banner flex flex-col justify-center items-center text-white'
-    style={{
-      backgroundImage: `url('${background}')`,
-      backgroundPosition: "center center",
-    }}
-  >
-    {/* TODO Add image overlay */}
-    <h4 className='text-5xl font-fjalla'>{children}</h4>
+const Banner = ({ src, alt, children }: BannerProps) => (
+  <div className='h-banner relative mt-20'>
+    <Image src={src} alt={alt} layout='fill' objectFit='cover' />
+    <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center text-white'>
+      <h4 className='text-5xl font-fjalla z-10'>{children}</h4>
+    </div>
+    <ImageOverlay />
   </div>
 );
 
