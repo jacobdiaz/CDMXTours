183df8ea705977c6597edb0537387f027381422f
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-06-24 18:28:54 -0500

    [chore] Setup env and headers to load sites url dynamically biketourcdmx

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    DOMAIN_URL: process.env.DOMAIN_URL,
+    DOMAIN_NAME: process.env.DOMAIN_NAME,
   },
   images: {
-    domains: ['d252kj1i9nz0td.cloudfront.net', 'lh3.googleusercontent.com'],
+    domains: ["d252kj1i9nz0td.cloudfront.net", "lh3.googleusercontent.com"],
   },
   i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'en',
+    locales: ["en", "es"],
+    defaultLocale: "en",
   },
 };
 
--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 type HeaderProps = {
   children?: React.ReactNode;
@@ -12,61 +12,62 @@ type HeaderProps = {
 
 const PageHeader = ({
   children,
-  type = 'website',
-  page_img = 'https://d252kj1i9nz0td.cloudfront.net/pages/Home/home_hero.jpg',
-  keywords = 'CDMX Tours, Bike Tours Mexico City, CDMX Bike Tours, Tours En Bici, Mexico City bike tours, Mexico City Tours, Bike Tours CDMX, Bike Tours, Architecture Tours Mexico City, Taco Tours CDMX, Tours En Bici CDMX ',
-  url_path = '',
-  socialTitle = 'CDMX Bike Tours',
-  socialDescription = 'Discover Mexico City by bike with our guided tours. We offer a variety of tours to suit your needs. Book your tour today!',
-}: HeaderProps) => (
-  <Head>
-    <meta property="og:image" content={page_img} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-    <meta property="og:image:secure_url" content={page_img} />
-    <meta property="og:image:type" content="image/jpeg" />
-    <meta name="keywords" content={keywords} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta property="og:type" content={type} />
-    <meta name="twitter:title" content={socialTitle} />
-    <meta name="twitter:description" content={socialDescription} />
-    <meta name="twitter:image" content={page_img} />
-    <link rel="icon" href="/favicon.ico" />
-    <link
-      rel="alternate"
-      href={`https://www.cdmxtours.com/${url_path}`}
-      hrefLang="x-default"
-    />
-    <link
-      rel="alternate"
-      href={`https://www.cdmxtours.com/${url_path}`}
-      hrefLang="en"
-    />
-    <link
-      rel="alternate"
-      href={`https://www.cdmxtours.com/es/${url_path}`}
-      hrefLang="es"
-    />
-    <link rel="canonical" href={`https://www.cdmxtours.com/${url_path}`} />
+  type = "website",
+  page_img = "https://d252kj1i9nz0td.cloudfront.net/pages/Home/home_hero.jpg",
+  keywords = "CDMX Tours, bike tours, Bike Tours Mexico City, CDMX Bike Tours, Tours En Bici, Mexico City bike tours, Mexico City Tours, Bike Tours CDMX, Bike Tours, Architecture Tours Mexico City, Taco Tours CDMX, Tours En Bici CDMX ",
+  url_path = "",
+  socialTitle = "CDMX Bike Tours",
+  socialDescription = "Discover Mexico City by bike with our guided tours. We offer a variety of tours to suit your needs. Book your tour today!",
+}: HeaderProps) => {
+  // Domain URL is https://www.cdmxtours.com or https://biketourcdmx.com
+  const domainUrl = process.env.DOMAIN_URL;
 
-    <link rel="apple-touch-icon" href="/apple-touch-icon-iphone-60x60.png" />
-    <link
-      rel="apple-touch-icon"
-      sizes="60x60"
-      href="/apple-touch-icon-ipad-76x76.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="114x114"
-      href="/apple-touch-icon-iphone-retina-120x120.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="144x144"
-      href="/apple-touch-icon-ipad-retina-152x152.png"
-    />
-    {children}
-  </Head>
-);
+  return (
+    <Head>
+      <meta property='og:image' content={page_img} />
+      <meta property='og:image:width' content='1200' />
+      <meta property='og:image:height' content='630' />
+      <meta property='og:image:secure_url' content={page_img} />
+      <meta property='og:image:type' content='image/jpeg' />
+      <link
+        rel='alternate'
+        href={`${domainUrl}/${url_path}`}
+        hrefLang='x-default'
+      />
+      <link rel='alternate' href={`${domainUrl}/${url_path}`} hrefLang='en' />
+      <link
+        rel='alternate'
+        href={`${domainUrl}/es/${url_path}`}
+        hrefLang='es'
+      />
+      <link rel='canonical' href={`${domainUrl}/${url_path}`} />
+      <meta property='og:url' content={`${domainUrl}/${url_path}`} />
+      <meta name='keywords' content={keywords} />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta property='og:type' content={type} />
+      <meta name='twitter:title' content={socialTitle} />
+      <meta name='twitter:description' content={socialDescription} />
+      <meta name='twitter:image' content={page_img} />
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='apple-touch-icon' href='/apple-touch-icon-iphone-60x60.png' />
+      <link
+        rel='apple-touch-icon'
+        sizes='60x60'
+        href='/apple-touch-icon-ipad-76x76.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='114x114'
+        href='/apple-touch-icon-iphone-retina-120x120.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='144x144'
+        href='/apple-touch-icon-ipad-retina-152x152.png'
+      />
+      {children}
+    </Head>
+  );
+};
 
 export default PageHeader;
