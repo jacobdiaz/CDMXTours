ad86cc2283248435c415bb910d7d979f54a7f5cc
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2024-03-06 18:56:02 -0600

    [refactor]: Add english and espanol btns and their locales

--- src/components/sections/AboutUsSection.tsx
+++ src/components/sections/AboutUsSection.tsx
@@ -5,23 +5,23 @@ import ImageOverlay from "../layout/ImageOverlay";
 const AboutUsSection = () => (
   <section>
     {/* Row 1 */}
-    <div className="grid md:grid-cols-2	">
+    <div className='grid md:grid-cols-2	'>
       {/* Col 1 */}
-      <div className="hidden md:block w-full h-full">
+      <div className='hidden md:block w-full h-full'>
         <Image
-          className="w-full object-cover max-h-[30rem] md:max-h-[800px]"
-          src="https://d252kj1i9nz0td.cloudfront.net/gallery_images/Architectural/arch_1v2.jpg"
-          alt="Image of Mexico City Bike Tour Guide"
+          className='w-full object-cover max-h-[30rem] md:max-h-[800px]'
+          src='https://d252kj1i9nz0td.cloudfront.net/gallery_images/Architectural/arch_1v2.jpg'
+          alt='Image of Mexico City Bike Tour Guide'
           height={720}
           width={720}
-          loading="lazy"
+          loading='lazy'
         />
       </div>
-      <div className="w-full flex flex-col justify-center items-center p-5 md:p-16 text-center">
+      <div className='w-full flex flex-col justify-center items-center p-5 md:p-16 text-center'>
         {/* Col 2 */}
         <SectionTitle>Architecture Bike Tours</SectionTitle>
 
-        <p className="text-lg text-primary">
+        <p className='text-lg text-primary'>
           Somos un grupo de arquitectos enfocados en dar los mejores Tours en
           Bici en CDMX. Ven y descubre nuestras rutas especializadas en
           arquitectura, historia, gastronomía y urbanismo en lugares como el
@@ -29,7 +29,7 @@ const AboutUsSection = () => (
           Coyoacán, Roma, Condesa y el bosque de Chapultepec.
         </p>
         <br />
-        <p className="text-lg text-primary italic">
+        <p className='text-lg text-primary italic'>
           We are a group of architects focused on giving the best Bike Tours in
           Mexico City. Come and discover our specialized routes in architecture,
           history, gastronomy and urbanism in places such as the Historic
@@ -37,49 +37,6 @@ const AboutUsSection = () => (
           monumental sites such as Chapultepec park.
         </p>
       </div>
-    </div>
-
-    {/* Row 2 */}
-    <div className="grid grid-cols-1 md:grid-cols-2 h-[800px] md:h-[30rem] overflow-hidden">
-      <a className="relative" href="https://www.cdmxtours.com/es/book-a-tour ">
-        <Image
-          src="https://d252kj1i9nz0td.cloudfront.net/pages/Home/btn-toursenbici.jpeg"
-          alt="Architectural Bike Tour in Mexico City"
-          className="w-full h-full absolute object-cover md:object-center"
-          height={540}
-          style={{ objectPosition: "center 70%" }}
-          width={720}
-          loading="lazy"
-        />
-        <ImageOverlay />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <p className="text-4xl font-bold text-white text-center font-spartan">
-            TOURS EN BICI <br />
-            (Español)
-          </p>
-        </div>
-      </a>
-      <a
-        className="relative mt-10 md:mt-0"
-        href="https://www.cdmxtours.com/book-a-tour "
-      >
-        <Image
-          src="https://d252kj1i9nz0td.cloudfront.net/pages/Home/btn-biketourscdmx.jpeg"
-          alt="Image of the Roma Condesa Bike Tour CDMX"
-          className="w-full h-full absolute object-cover"
-          height={540}
-          width={720}
-          style={{ objectPosition: "center 70%" }}
-          loading="lazy"
-        />
-        <ImageOverlay />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <p className="text-4xl font-bold text-white text-center">
-            BIKE TOURS <br />
-            (English)
-          </p>
-        </div>
-      </a>
     </div>
   </section>
 );
--- src/components/sections/ToursSection.tsx
+++ src/components/sections/ToursSection.tsx
@@ -3,6 +3,7 @@ import { Tours } from "@/utils/toursdata";
 import { FormattedMessage, useIntl } from "react-intl";
 import SectionTitle from "./SectionTitle";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const TourSection = ({ hideHeader = false }: { hideHeader?: boolean }) => {
   const Intl = useIntl();
@@ -15,25 +16,37 @@ const TourSection = ({ hideHeader = false }: { hideHeader?: boolean }) => {
   return (
     <section>
       {!hideHeader && (
-        <div className="flex flex-col justify-center md:items-center mt-10 mb-5">
-          <SectionTitle margin="m-0">
-            <FormattedMessage id="home.tours.title" />
+        <div className='flex flex-col justify-center md:items-center mt-10 mb-5'>
+          <SectionTitle margin='m-0'>
+            <FormattedMessage id='home.tours.title' />
           </SectionTitle>
         </div>
       )}
-      <div className="flex flex-col justify-center items-center w-full">
-        {!hideHeader && (
-          <p className="text-lg mb-10">
-            <FormattedMessage id="home.tours.description" />
-          </p>
-        )}
-    <ul className="flex flex-wrap justify-center md:justify-between w-full">
-      {filteredTours.map((t, idx) => (
-        <li className="w-[95%] md:w-auto px-2" key={idx}>
-          <TourCard {...t} key={t.id.toString()} />
-        </li>
-      ))}
-    </ul>
+      {/* Row of english and espanol bottons */}
+      <div className='flex flex-row justify-between mb-10'>
+        <Link
+          href='/book-a-tour'
+          locale='en'
+          className='w-[48%] p-5 bg-white text-black text-center border-2 border-black'
+        >
+          English
+        </Link>
+        <Link
+          href='/book-a-tour/'
+          locale='es'
+          className='w-[48%] p-5 bg-black text-white  text-center'
+        >
+          Español
+        </Link>
+      </div>
+      <div className='flex flex-col justify-center items-center w-full'>
+        <ul className='flex flex-wrap justify-center md:justify-between w-full'>
+          {filteredTours.map((t, idx) => (
+            <li className='w-[95%] md:w-auto px-2' key={idx}>
+              <TourCard {...t} key={t.id.toString()} />
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
